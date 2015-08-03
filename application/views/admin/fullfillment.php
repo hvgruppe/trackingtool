b4b75eb59d9f784ca1777db32ca4043f342fbdbc
unknown <selva316@gmail.com>
2015-08-03 11:38:54 +0530

    Themes has been chamged for DataTables

--- application/views/admin/fullfillment.php
+++ application/views/admin/fullfillment.php
@@ -1,10 +1,198 @@
-<?php
-	if($this->session->userdata('roleid')!=1){
-		// echo site_url('login');
-		print "<script>window.location.href='".site_url('login')."';</script>";
-	}
-?>
-<?php $this->load->view('includes/adminconfiguration');?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Homepage</title>
+    <!-- Bootstrap Core CSS -->
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/bootstrap/dist/css/bootstrap.min.css" />
+	<!-- Bootstrap Responsive CSS -->
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/bootstrap/bootstrap-responsive.min.css" />
+	<!-- MetisMenu CSS -->
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/metisMenu/dist/metisMenu.min.css" />
+    <!-- Timeline CSS -->
+	<link rel="stylesheet" href="<?php echo base_url();?>dist/css/timeline.css" />
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="<?php echo base_url();?>dist/css/sb-admin-2.css" />
+	<!-- Custom Fonts -->
+    <link href="<?php echo base_url();?>assets/font-awesome/css/font-awesome.min.css" />
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	
+	<?php 
+	foreach($css_files as $file): ?>
+		<link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
+	<?php endforeach; ?>
+	<?php foreach($js_files as $file): ?>
+		<script src="<?php echo $file; ?>"></script>
+	<?php endforeach; ?>
+	
+	<style>
+		/*
+		.navbar-header{
+			margin-bottom:40px;
+		}*/
+	</style>
+</head>
+
+<body>
+
+    <div id="wrapper">
+		
+		<!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+				<a class="navbar-brand" href=<?php echo site_url('admin/homepage');?>>
+					<img src="<?php echo base_url();?>img/gizmoland.png" alt="gizmoland" />
+				</a>
+            </div>
+            <!-- /.navbar-header -->
+            <ul class="nav navbar-top-links navbar-right">
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <!--<li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                        </li>
+                        <li class="divider"></li>-->
+                        <li><a href=<?php echo site_url('login');?>><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-user -->
+                </li>
+                <!-- /.dropdown -->
+            </ul>
+            <!-- /.navbar-top-links -->
+
+            <div class="navbar-default sidebar" role="navigation" style="margin-top:90px;">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li class="sidebar-search">
+                            <!--<div class="input-group custom-search-form">
+                                <input type="text" class="form-control" placeholder="Search...">
+                                <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                            </span>
+                            </div>-->
+                            <!-- /input-group -->
+                        </li>
+                        <li>
+                            <a href="homepage"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-files-o fa-fw"></i>Configuration <span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+								<li>
+									<a href=<?php echo site_url('admin/fullfillment');?>>Fullfillment</a>
+								</li>
+								<li>
+									<a href=<?php echo site_url('admin/disposition');?>>Disposition</a>
+								</li>
+								<li>
+									<a href=<?php echo site_url('admin/productcondition');?>>Product Condition</a>
+								</li>
+								<li>
+									<a href=<?php echo site_url('admin/productstatus');?>>Product Status</a>
+								</li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <!--<li>
+                            <a href="tables.html"><i class="fa fa-table fa-fw"></i> Tables</a>
+                        </li>
+                        <li>
+                            <a href="forms.html"><i class="fa fa-edit fa-fw"></i> Forms</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-wrench fa-fw"></i> UI Elements<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="panels-wells.html">Panels and Wells</a>
+                                </li>
+                                <li>
+                                    <a href="buttons.html">Buttons</a>
+                                </li>
+                                <li>
+                                    <a href="notifications.html">Notifications</a>
+                                </li>
+                                <li>
+                                    <a href="typography.html">Typography</a>
+                                </li>
+                                <li>
+                                    <a href="icons.html"> Icons</a>
+                                </li>
+                                <li>
+                                    <a href="grid.html">Grid</a>
+                                </li>
+                            </ul> -->
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <!-- <li>
+                            <a href="#"><i class="fa fa-sitemap fa-fw"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="#">Second Level Item</a>
+                                </li>
+                                <li>
+                                    <a href="#">Second Level Item</a>
+                                </li>
+                                <li>
+                                    <a href="#">Third Level <span class="fa arrow"></span></a>
+                                    <ul class="nav nav-third-level">
+                                        <li>
+                                            <a href="#">Third Level Item</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Third Level Item</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Third Level Item</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Third Level Item</a>
+                                        </li>
+                                    </ul>
+                                    
+                                </li>
+								
+                            </ul> 
+                            
+                        </li>
+						<!-- /.nav-second-level -->
+                        <!-- <li>
+                            <a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="blank.html">Blank Page</a>
+                                </li>
+                                <li>
+                                    <a href="login.html">Login Page</a>
+                                </li>
+                            </ul>
+                            
+                        </li><!-- /.nav-second-level -->
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
 	<div id="page-wrapper">
 		<div class="row">
 			<div class="col-lg-12">
@@ -16,8 +204,8 @@
 		<div class="panel panel-default">
 			<div class="panel-heading">List of Fullfillment</div>
 			<div class="panel-body">
-			<?php echo $output; ?>
-		</div>
+				<?php echo $output; ?>
+			</div>
 		</div> 
 	   
 	</div>
